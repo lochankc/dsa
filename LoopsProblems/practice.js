1cb3be366b374dd8641068db8a69ae92dd3acf90
Lochan K C <lochankc97@gmail.com>
2026-01-28 11:23:43 +0530

    find largest in the array

---
+++ LoopsProblems/practice.js
@@ -0,0 +1,15 @@
+// Find the largest element in the Array
+
+let arr = [-2, 4, 2, 9, 45, 28, -3, -5];
+
+function findLargest(arr) {
+    let largest = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]
+        }
+    }
+
+    return largest;
+}
