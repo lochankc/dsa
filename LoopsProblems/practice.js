739b0e5b931b7b47d0c01848940b6ac50b4b4b4b
Lochan K C <lochankc97@gmail.com>
2026-01-31 21:39:24 +0530

    start patterns

--- LoopsProblems/practice.js
+++ LoopsProblems/practice.js
@@ -103,3 +103,34 @@ for (let i = 0; i < 5; i++) {
     }
     console.log(row)
 }
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+for (let i = 5; i > 0; i--) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+        row = row + (j + 1)
+    }
+    console.log(row)
+}
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+let n = 5
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i -1; j++) {
+        row = row + " ";
+    }
+    for (let k = 0; k <= i; k++) {
+        row = row + "*"
+    }
+    console.log(row)
+}
