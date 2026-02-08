e8edda57aa7799e3b60a046d2d7693c825b24b3b
Lochan K C <lochankc97@gmail.com>
2026-02-08 20:05:30 +0530

    Best Time to Buy and Sell Stock

--- LoopsProblems/practice.js
+++ LoopsProblems/practice.js
@@ -292,3 +292,24 @@ function reverseString(s) {
 // Input Value
 // let s = ["h", "e", "l", "l", "o"];
 console.log(reverseString(s))
+
+// Best Time to Buy and Sell Stock.
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+function maxProfit(prices) {
+    let min = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] - min > maxProfit) {
+            maxProfit = prices[i] - min;
+        }
+        if (prices[i] < min) {
+            min = prices[i]
+        }
+    }
+
+    return maxProfit
+}
+// inputValue
+// let prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
