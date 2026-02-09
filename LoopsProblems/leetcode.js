e4825ff4a54a82c3d60bc73980142e6c770aa18c
Lochan K C <lochankc97@gmail.com>
2026-02-09 20:55:59 +0530

    Move Zeroes

--- LoopsProblems/leetcode.js
+++ LoopsProblems/leetcode.js
@@ -74,3 +74,23 @@ function maxProfit(prices) {
 // inputValue
 // let prices = [7, 1, 5, 3, 6, 4];
 console.log(maxProfit(prices));
+
+// Move Zeroes
+// https://leetcode.com/problems/move-zeroes/description/
+var moveZeroes = function(nums) {
+    let x = 0;
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[x] === 0) {
+            if(nums[i] !== 0) {
+                [nums[x], nums[i]] = [nums[i], nums[x]];
+                x = x + 1;
+            }
+        } else {
+            x = x + 1;
+        }
+    }
+};
+
+// Input Value.
+// let nums = [0,1,0,3,12]
+console.log(moveZeroes(nums))
